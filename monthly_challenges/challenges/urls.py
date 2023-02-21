df2cb602708848419432dfa2d884d5bc034349d0
PROJECT-DEFIANT <szymonszyszkowski@gmail.com>
2023-02-21 14:23:16 +0100

    first route

---
+++ monthly_challenges/challenges/urls.py
@@ -0,0 +1,6 @@
+"""Urls."""
+from django.urls import path
+
+from .views import index
+
+urlpatterns = [path("january", index)]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
 """Create views here."""
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    """Index page for challenges app.
+
+    Args:
+        request (HttpRequest): incoming request
+
+    Returns:
+        HttpResponse: response
+    """
+    print(request)
+    return HttpResponse("This works!")
